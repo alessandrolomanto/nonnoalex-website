1cb729883dfaab1125c6496eb955dabc0b67cbe4
Alessandro Lo Manto <alessandro.lo.manto@gmail.com>
2021-07-19 15:57:51 +0200

    Updating script for GitOps testing env

--- build.sh
+++ build.sh
@@ -5,8 +5,7 @@ read version
 
 echo "Preparing build: $version";
 docker build -t nonnoalex-website:$version .
-docker tag nonnoalex-website:$version localhost:5000/nonnoalex-website:$version
-docker push localhost:5000/nonnoalex-website:$version
+docker tag nonnoalex-website:$version registry.nonnoalex.dev/nonnoalex/nonnoalex-website:$version
+docker push registry.nonnoalex.dev/nonnoalex/nonnoalex-website:$version
 
 exit 0
-
